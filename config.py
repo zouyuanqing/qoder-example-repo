efd2ac25a25a11424a5cfe72857840f7318d1e7c
zouyuanqing <zou.yuanqing@foxmail.com>
2025-09-25 13:25:44 +0800

    Add configuration file

---
+++ config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Configuration file for Qoder Example Project
+"""
+
+# Project configuration
+PROJECT_NAME = "Qoder Example Project"
+VERSION = "1.0.0"
+AUTHOR = "Qoder IDE User"
+
+# Configuration settings
+DEBUG = True
+LOG_LEVEL = "INFO"
+
+# Default values
+DEFAULT_GREETING = "Welcome to the Qoder example repository!"
